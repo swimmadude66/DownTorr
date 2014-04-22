4a06d361d5293007f67a51113124f7e136bef24d
Swimmadude66 <swimmadude66@gmail.com>
2014-04-22 19:06:44 -0400

    Added URL encoding and new small torrents

--- Bencoding.c
+++ Bencoding.c
@@ -236,7 +236,6 @@ Torrent* parse_torrent(char *input, long limit){
 		else if(!strcmp(d->key,"pieces")){
 		  t->pieces = d->value->cargo.str.string;
   		  t->pieces_size = d->value->cargo.str.length;
-		  printf("Size of Pieces: %d\n",t->pieces_size);
 		  d = d->next;
 		}
 		else if(!strcmp(d->key,"url-list")){
@@ -372,6 +371,59 @@ void print_bencoding(Bencoding *b, int indent)
 			break;
 	}
 }
+
+
+/*URL Encoder courtesy of GeekHideout*/
+
+/* Converts a hex character to its integer value */
+char from_hex(char ch) {
+  return isdigit(ch) ? ch - '0' : tolower(ch) - 'a' + 10;
+}
+
+/* Converts an integer value to its hex character*/
+char to_hex(char code) {
+  static char hex[] = "0123456789abcdef";
+  return hex[code & 15];
+}
+
+/* Returns a url-encoded version of str */
+/* IMPORTANT: be sure to free() the returned string after use */
+char *url_encode(char *str) {
+  char *pstr = str, *buf = malloc(strlen(str) * 3 + 1), *pbuf = buf;
+  while (*pstr) {
+    if (isalnum(*pstr) || *pstr == '-' || *pstr == '_' || *pstr == '.' || *pstr == '~') 
+      *pbuf++ = *pstr;
+    else if (*pstr == ' ') 
+      *pbuf++ = '+';
+    else 
+      *pbuf++ = '%', *pbuf++ = to_hex(*pstr >> 4), *pbuf++ = to_hex(*pstr & 15);
+    pstr++;
+  }
+  *pbuf = '\0';
+  return buf;
+}
+
+/* Returns a url-decoded version of str */
+/* IMPORTANT: be sure to free() the returned string after use */
+char *url_decode(char *str) {
+  char *pstr = str, *buf = malloc(strlen(str) + 1), *pbuf = buf;
+  while (*pstr) {
+    if (*pstr == '%') {
+      if (pstr[1] && pstr[2]) {
+        *pbuf++ = from_hex(pstr[1]) << 4 | from_hex(pstr[2]);
+        pstr += 2;
+      }
+    } else if (*pstr == '+') { 
+      *pbuf++ = ' ';
+    } else {
+      *pbuf++ = *pstr;
+    }
+    pstr++;
+  }
+  *pbuf = '\0';
+  return buf;
+}
+
 
 /*
 int main(int argc, char *argv[])
