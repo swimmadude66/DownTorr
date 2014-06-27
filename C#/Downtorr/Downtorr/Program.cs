1eceb81dff7aa11e623c6afb443ac999e01e3b2b
Adam Yost <swimmadude66@gmail.com>
2014-06-27 10:08:53 -0400

    Added C# stubs

---
+++ C#/Downtorr/Downtorr/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Downtorr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TorrentModel model = BDecode(file);
+            //Announce ann = parseAnnounce(mnodel);
+            //PeerResponse r = get(ann);
+            //List<Peer> peers = BDecode(r);
+            //for (Peer p : peers){
+                //Response r = handshake(peer);
+                //download(r);
+            //}
+        }
+    }
+}
